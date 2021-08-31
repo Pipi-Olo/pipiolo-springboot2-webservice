e1ad75141a1446bd5d6916400fcaa55aa06cc7e4
MOON <32050398+GEERUP@users.noreply.github.com>
2021-08-31 18:25:58 +0900

    update login using google ID

---
+++ src/main/java/com/pipiolo/springboot/domain/user/Role.java
@@ -0,0 +1,16 @@
+package com.pipiolo.springboot.domain.user;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum Role {
+
+    GUEST("ROLE_GUEST", "GUEST"),
+    USER("ROLE_USER", "USER");
+
+    private final String key;
+    private final String title;
+}
